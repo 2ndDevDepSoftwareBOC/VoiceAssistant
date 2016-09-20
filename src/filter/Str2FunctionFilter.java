193529a27c537f30d76801c2791cc18996585688
jdnull <jiaduan1990@163.com>
2016-09-20 17:00:41 +0800

    完善处理流程

--- src/filter/Str2FunctionFilter.java
+++ src/filter/Str2FunctionFilter.java
@@ -1,6 +1,7 @@
 package filter;
 
 import java.io.IOException;
+import java.util.HashMap;
 
 import javax.servlet.DispatcherType;
 import javax.servlet.Filter;
@@ -10,6 +11,9 @@ import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.annotation.WebFilter;
+
+import nlp.*;
+import workFlow.*;
 
 @WebFilter(urlPatterns={"/engine/*"}, asyncSupported=true, dispatcherTypes={DispatcherType.REQUEST}
          )  
@@ -33,8 +37,20 @@ public class Str2FunctionFilter implements Filter {
       
       String plainStr = (String) request.getAttribute("plainStr");
       
+      TLPAllTaskProcessor tlpProcessor = new TLPAllTaskProcessor();
+      String jsonStr = tlpProcessor.process(plainStr);
       
-      
+      NLPParser nlpParser = new NLPParser(); 
+      String rootWord = nlpParser.getRootWord(jsonStr);
+
+      WordVector wordVector = new WordVector();
+      String functionName = wordVector.matchWordSet(rootWord); // 硬匹配，后面改word2vec
+
+      HashMap<String, String> functionElementMap = null;
+      if (functionName.equals("转账")) { // 根据rootword确定业务规则
+
+            functionElementMap = ((CrossBankNLPParser)nlpParser).execute(plainStr, functionName, jsonStr);
+      }
    }  
    
    @Override  
