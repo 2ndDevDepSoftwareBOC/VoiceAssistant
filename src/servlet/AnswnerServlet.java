84ddab6025ab292bcdd4d8684a29e87e954f7968
jdnull <jiaduan1990@163.com>
2016-09-23 09:28:33 +0800

    修改answerServlet，只识别，识别结果直接返回

--- src/servlet/AnswnerServlet.java
+++ src/servlet/AnswnerServlet.java
@@ -1,7 +1,9 @@
 package servlet;
 
 
+import java.io.File;
 import java.io.IOException;
+import java.util.List;
 
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
@@ -9,6 +11,13 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.fileupload.FileItem;
+import org.apache.commons.fileupload.FileUploadException;
+import org.apache.commons.fileupload.disk.DiskFileItemFactory;
+import org.apache.commons.fileupload.servlet.ServletFileUpload;
+
+import com.jdnull.speechRec.baiduAPI.Recognizer;
 
 @WebServlet("/voiceAssistant/answner")
 public class AnswnerServlet extends HttpServlet {
@@ -33,11 +42,55 @@ public class AnswnerServlet extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
 		request.setCharacterEncoding("UTF-8"); 
-		ServletContext sct=getServletConfig().getServletContext();   
+//		ServletContext sct=getServletConfig().getServletContext();   
 
-		String answner = (String) request.getAttribute("answner");
+//		String answner = (String) request.getAttribute("answner");
+		
+		DiskFileItemFactory factory = new DiskFileItemFactory();
+
+		// 如果没以下两行设置的话，上传大的文件会占用很多内存，
+		// 设置暂时存放的存储室 ,这个存储室,可以和最终存储文件的目录不同
+		/**
+		 * 原理 它是先存到 暂时存储室，然后在真正写到 对应目录的硬盘上， 按理来说 当上传一个文件时，其实是上传了两份，第一个是以 .tmp
+		 * 格式的 然后再将其真正写到 对应目录的硬盘上
+		 */
+		String filename = "voice.wav";
+		factory.setRepository(new File(filename));
+		// 设置 缓存的大小，当上传文件的容量超过该缓存时，直接放到 暂时存储室
+		factory.setSizeThreshold(1024 * 1024);
+
+		// 高水平的API文件上传处理
+		ServletFileUpload upload = new ServletFileUpload(factory);
+
+		try {
+			// 提交上来的信息都在这个list里面
+			// 这意味着可以上传多个文件
+			// 请自行组织代码
+			List<FileItem> list = upload.parseRequest((HttpServletRequest) request);
+			// 获取上传的文件
+			FileItem item = list.get(0);
+			// 真正写到磁盘上
+			item.write(new File(filename)); // 第三方提供的
+			// 输出信息,前端页面获取,这里用的json格式
+
+		} catch (FileUploadException e) {
+			e.printStackTrace();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		String recResult = "";
+		Recognizer rec = new Recognizer();
+		try {
+			recResult = rec.recognize(filename);
+//			request.setAttribute("originalStr", recResult);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 		//保存输入
-		response.getWriter().print("{text:\""+answner+"\"}");
+		response.getWriter().print("{answner:\""+recResult+"\"}");
 
 	}
 
